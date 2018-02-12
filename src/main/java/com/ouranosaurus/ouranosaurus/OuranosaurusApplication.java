7a11d2a4b101f0713e0c64d396013abec61ad919
zhouJH <1123678582@qq.com>
2018-02-12 14:56:05 +0800

    Initial commit

---
+++ src/main/java/com/ouranosaurus/ouranosaurus/OuranosaurusApplication.java
@@ -0,0 +1,12 @@
+package com.ouranosaurus.ouranosaurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuranosaurusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuranosaurusApplication.class, args);
+	}
+}
